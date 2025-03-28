b2f12817cf5de2e3d37cf105715af7acca657ff6
Minesh Patel <minesh13096@gmail.com>
2025-03-28 19:49:31 +0530

    Data Ingestion and Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -23,7 +23,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv('Notebook\final_data.csv')
+            df=pd.read_csv(r'C:\Users\lenovo\OneDrive\Desktop\Intellipaat\LeadScorePrediction\Notebook\final_data.csv')
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
@@ -52,7 +52,7 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
 
 
